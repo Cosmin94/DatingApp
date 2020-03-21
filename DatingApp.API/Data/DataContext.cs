f7fb7b3af1f046ace2cf87812eacc043905f7578
Cosmin94 <cosmin.salca@maxcode.net>
2020-03-21 18:24:54 +0200

    Chapter 3 part 5

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -5,9 +5,10 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options): base(options) {}    
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-    public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+        public DbSet<User> Users { get; set; }
 
     }
 
