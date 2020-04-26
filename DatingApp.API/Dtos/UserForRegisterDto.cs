31f80660cf5116c8038f37469dacbf3db541cb24
Cosmin94 <cosmin.salca@maxcode.net>
2020-04-26 22:16:13 +0300

    Chapter 3 part 11 Add validation for register user dto

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(24, MinimumLength = 8, ErrorMessage = "You mush specify a password between 8 and 24")]
         public string Password { get; set; }
     }
 }
